dfc2c53ab0947f6f25548141f440820b08b914ac
Tom Wilhoit <tomwilhoit.business@gmail.com>
2019-04-13 12:58:42 -0600

    Creates more data for local

--- app.js
+++ app.js
@@ -4,13 +4,41 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-app.locals.locations = ["Metropolis", "Wakanda", "Hogwarts"];
+app.locals.locations = [
+  {
+    ficName: "Metropolis",
+    realName: "New York City",
+    id: 0,
+    lat: 40.7128,
+    long: -74.006,
+    cat: "DC",
+    reason: "Ipso Lorum"
+  },
+  {
+    ficName: "Wakanda",
+    realName: "Democratic Republic of the Congo",
+    id: 1,
+    lat: 4.8594,
+    long: 31.5713,
+    cat: "Marvel",
+    reason: "Ipso Lorum"
+  },
+  {
+    ficName: "Hogwarts",
+    realName: "Northern Scotland",
+    id: 2,
+    lat: 57.4216,
+    long: 4.6656,
+    cat: "Harry Potter",
+    reason: "Ipso Lorum"
+  }
+];
 
-app.set('port', process.env.PORT || 3000);
-app.locals.title = 'Feather';
+app.set("port", process.env.PORT || 3000);
+app.locals.title = "Feather";
 
 app.get("/api/v1/locations", (request, response) => {
   response.status(200).json(app.locals.locations);
 });
 
-export default app
+export default app;
